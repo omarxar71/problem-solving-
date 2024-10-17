042236f43f5e77c599c71533082cf87e2cbf23be
omarxar71 <omarabdrabi123@gmail.com>
2024-10-17 14:27:20 +0300

    commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// exercise-1 
+// function even  (array){
+//     for (let i =0 ; i <array.length ; i++){
+//         if(array[i] % 2 ==0){
+//             let newArray = array[i];
+//             return newArray
+            
+//         }
+//     }
+    
+// }
+
+
+
+
+
+// exercise 2 
+
+// function largest (array){
+//     largestNumber=Math.max(...array)
+//     console.log(largestNumber)
+//     return largestNumber;
+// }
+// let result = largest([7,3,4,5]);
+// console.log(result);
+
+
+
+
+// exercise 3
+// function reversed (array){
+//     array.reverse()
+//     return array
+// }
+// let result=reversed(['omar' , 'ahmed'])
+// console.log(result)
+
+
+
+
+
+
+
+
+
+// // exercise 4 
+// function removeDuplicates(arr) {
+//     let uniqueNumbers = [];
+//     for(let i = 0 ; i< arr.length ; i++){
+//        isDub =false;
+//        for(let j = 0 ; j < uniqueNumbers.length ; i++){
+//         if(arr[i]== uniqueNumbers[j]){
+//             isDub = true;
+//         }
+
+       
+//        }
+//        if(!isDub){
+//         uniqueNumbers.push(arr[i]);  
+//     }
+//     }
+//   }
+//     let number = [3,34,5,5,6,1,3,3,3,5,5,5]
+//     let result = removeDuplicates(number)
+//     console.log(result)    
+
